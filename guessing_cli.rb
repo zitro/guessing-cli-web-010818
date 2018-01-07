d1d8b644831d7c6c5a258c55ae6a91879bb3ab8e
Bryan Ortiz <bryan@bryanortiz.com>
2018-01-07 14:01:39 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,4 +3,5 @@
 #1. get.chomp
 #2. compare input of random number that is generated
 #3. puts out state "You guessed the correct number!" if the number has been gussed correctly or "The computer guessed #{number}" for incorrect answers.
-#if exit is input user extis program
+#4. if exit is input user extis program
+#.5 use while and randoms and break
