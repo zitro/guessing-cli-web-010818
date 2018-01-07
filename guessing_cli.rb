56c4ec4f532b79bb32d493154740f53a69c696f1
Bryan Ortiz <bryan@bryanortiz.com>
2018-01-07 19:41:39 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -13,7 +13,6 @@ puts "Guess a number between 1 and 6."
   user_guess = gets.chomp
   if user_guess.downcase == "exit"
     puts "Goodbye!"
-    break
   elsif user_guess.to_i == randomNumber
     puts "You guessed the correct number!"
     
