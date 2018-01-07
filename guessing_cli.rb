a80268a3fe9a2f17e4674cfe9affaf8e5bc23ad1
Bryan Ortiz <bryan@bryanortiz.com>
2018-01-07 19:19:48 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,3 +5,12 @@
 #3. puts out state "You guessed the correct number!" if the number has been gussed correctly or "The computer guessed #{number}" for incorrect answers.
 #4. if exit is input user extis program
 #.5 use while and randoms and break
+
+
+def run_guessing_game
+puts "Guess a number between 1 and 6."
+  user_guess = gets.chomp
+  if user_guess.downcase == "exit"
+  end
+else
+  
